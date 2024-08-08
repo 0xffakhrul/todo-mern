44a2ee5e728ff7bfd542af8679e0476f2139d4c8
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-08-08 17:58:32 +0800

    custom hooks and data fetching

--- client/src/api/todo.ts
+++ client/src/api/todo.ts
@@ -1,13 +1,43 @@
-// import { useQueries } from "@tanstack/react-query";
-// import { getTodo } from "./request";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createTodo, deleteTodo, getTodos, Todo, updateTodo } from "./request";
 
-// export function fetchTodos(userId: (string | undefined)[] | undefined) {
-//   return useQueries({
-//     queries: (userId ?? []).map((id) => {
-//       return {
-//         queryKey: ["todo", id],
-//         queryFn: () => getTodo(id!),
-//       };
-//     }),
-//   });
-// }
+export const useTodos = (userId: string | undefined) => {
+  return useQuery<Todo[], Error>({
+    queryKey: ["todos", userId],
+    queryFn: () => getTodos(userId ?? ""),
+    enabled: !!userId,
+  });
+};
+
+export const useCreateTodo = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createTodo,
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({ queryKey: ["todos", variables.userId] });
+    },
+  });
+};
+
+export const useUpdateTodo = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateTodo,
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({ queryKey: ["todos", variables.userId] });
+    },
+  });
+};
+
+export const useDeleteTodo = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteTodo,
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({ queryKey: ["todos", variables.userId] });
+    },
+  });
+};
