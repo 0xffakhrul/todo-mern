235ab12bcf5e222cff354559a726a423b5f026dc
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-08-09 17:05:58 +0800

    styling landing page

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -8,10 +8,23 @@ import {
 
 const Auth = () => {
   return (
-    <div>
+    <div className="max-w-7xl min-h-screen mx-auto flex flex-col items-center justify-center py-12 sm:py-24 lg:py-32">
+      <h1 className="pb-8 bg-gradient-to-l from-cyan-200 to-purple-400 bg-clip-text text-transparent text-7xl font-black text-center">
+        Start Todoing now.
+      </h1>
       <SignedOut>
-        <SignUpButton mode="modal" />
-        <SignInButton mode="modal" />
+        <div className="space-x-4">
+          <SignUpButton mode="modal">
+            <button className="px-6 py-2 bg-purple-500/40 rounded-lg text-lg font-black text-purple-300">
+              Sign Up
+            </button>
+          </SignUpButton>
+          <SignInButton mode="modal">
+            <button className="px-6 py-2 bg-green-500/40 rounded-lg text-lg font-black text-green-300 ">
+              Sign In
+            </button>
+          </SignInButton>
+        </div>
       </SignedOut>
 
       <SignedIn>
